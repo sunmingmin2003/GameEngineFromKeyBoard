400404a9a03943f67091caaffc6b68e0efe252ac
sunmingmin2003 <sunmingmin2003@126.com>
2018-03-07 09:34:11 +0800

    real memory manager

---
+++ Framework/Common/MemoryManager.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include "Allocator.hpp"
+#include "IRuntimeModule.hpp"
+#include <new>
+
+namespace My {
+class MemoryManager : implements IRuntimeModule {
+public:
+  template <typename T, typename... Arguments> T *New(Arguments... parameters) {
+    return new (Allocator(sizeof(T))) T(parameter...);
+  }
+
+  template <typename T> void Delete(T *p) {
+    reinterpret_cast<T *>(p)->~T();
+    Free(p, sizeof(T));
+  }
+
+public:
+  virtual ~MemoryManager() {}
+  virtual int Initialize();
+  virtual void Finalize();
+  virtual void Tick();
+
+  void *Allocate(size_t size);
+  void Free(void *p, size_t size);
+
+private:
+  static size_t *m_pBlockSizeLookup;
+  static Allocator *m_pAllocators;
+
+private:
+  static Allocator *LookUpAllocator(size_t size);
+};
+} // namespace My
